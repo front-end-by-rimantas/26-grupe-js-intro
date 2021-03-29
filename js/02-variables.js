9864708f285631b0b779397c50e9b0250f6c0890
Rimantas <belauzas@yahoo.com>
2021-03-29 18:06:06 +0300

    Variables

---
+++ js/02-variables.js
@@ -0,0 +1,47 @@
+/*
+Iniciavimas - vietos isskyrimas kompiuterio atmintyje.
+
+KINTAMUJU INICIAVIMO BUDAI:
+const - konstanta; kintamasis, kurio reiksme negali buti keiciama - DEFAULT;
+let - kintamasis, kurio reiksme gali buti keiciama;
+var - kintamasis, kurio reiksme gali buti keiciama, taciau nerekomenduojam naudoti;
+
+KINTAMUJU TIPAI:
+tekstas - (string) - rasomas tarp kabuciu;
+
+skaicius - (number) - rasomi skaiciai;
+
+sarasas, masyvas, listas, arejus - (array) - sarasas reiksmiu, kuriu tipas gali buti betkoks, taciau rekomenduotina, jog sarasa sudarytu vieno tipo reiksmes;
+*/
+
+const name = 'Petras';
+console.log(name);
+
+const lastName = 'Pavardenis';
+console.log(lastName);
+
+let balance = 99;
+console.log(balance);
+
+const curency = 'eur';
+console.log(curency);
+
+balance = 999;
+balance = 10;
+balance = -10;
+console.log(balance);
+
+const skaicius1 = 5;
+console.log(skaicius1);
+
+const skaicius2 = 7.46;
+console.log(skaicius2);
+
+const skaicius3 = -84.3;
+console.log(skaicius3);
+
+const pazymiai = [10, 2, 8, 6];
+console.log(pazymiai);
+
+const studentai = ['Petriukas', 'Onute', 'Maryte', 'Jonukas', 'Alphe', 'Ra'];
+console.log(studentai);
---
+++ js/03-veiksmai.js
@@ -0,0 +1,39 @@
+const nr1 = 8;
+const nr2 = 5;
+
+const sum = nr1 + nr2;
+console.log(sum);
+
+const minus = nr1 - nr2;
+console.log(minus);
+
+const multi = nr1 * nr2;
+console.log(multi);
+
+const divide = nr1 / nr2;
+console.log(divide);
+
+const combo = (nr1 + nr2) * (10 / nr2);
+console.log(combo);
+
+const eiliskumas = 1 + (2 - 4) * 5 / 10;
+console.log(eiliskumas);
+
+
+const firstName = 'Petras';
+const lastName = 'Petraitis';
+
+const fullName = firstName + ' ' + lastName + '!';
+console.log(fullName);
+
+const sayHi = 'Labas, ' + firstName + '!';
+console.log(sayHi);
+
+
+// parodom, jog kintamuju pavadinimai yra case-sensitive
+const FirstName = 'Jonas';
+
+
+console.log(firstName);
+console.log(FirstName);
+console.log(firstname);
