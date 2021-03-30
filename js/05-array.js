368ca0725fe0b52bb48bd32879c60d4b128904fd
Rimantas <belauzas@yahoo.com>
2021-03-30 15:46:55 +0300

    Array

---
+++ js/05-array.js
@@ -0,0 +1,16 @@
+/*
+
+*/
+
+const marks = [10, 2, 8, 4, 6];
+console.log(marks);
+
+const firstMark = marks[0];
+console.log('Pirmasis pazymys:', firstMark);
+
+const secondMark = marks[1];
+console.log('Antrasis pazymys:', secondMark);
+
+const lastMarkIndex = marks.length - 1;
+const lastMark = marks[lastMarkIndex];
+console.log('Paskutinis pazymys:', lastMark);
