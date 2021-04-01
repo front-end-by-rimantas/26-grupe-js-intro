124ad647f9558660bd8b721f793b00067c4d8beb
Rimantas <belauzas@yahoo.com>
2021-04-01 17:49:21 +0300

    functions

---
+++ codewars/positiveSum.js
@@ -0,0 +1,10 @@
+function positiveSum(arr) {
+    let suma = arr[0] + arr[1];
+    return suma;
+}
+
+console.log(positiveSum([1, 2, 3, 4, 5]), 15);
+console.log(positiveSum([1, -2, 3, 4, 5]), 13);
+console.log(positiveSum([]), 0);
+console.log(positiveSum([-1, -2, -3, -4, -5]), 0);
+console.log(positiveSum([-1, 2, 3, 4, -5]), 9);
---
+++ js/11-empty.js
@@ -0,0 +1,12 @@
+function tusciaFunkcija() {
+    return false;
+}
+
+console.log(tusciaFunkcija());
+console.log(tusciaFunkcija(14856));
+console.log(tusciaFunkcija(true));
+console.log(tusciaFunkcija('afsdg'));
+console.log(tusciaFunkcija([]));
+console.log(tusciaFunkcija([554]));
+console.log(tusciaFunkcija(['fd']));
+console.log(tusciaFunkcija(undefined));
